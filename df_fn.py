962b28baffe21f3e55df63f51e3f9b1cf2c0391b
Alex F. Bokov <bokov@uthscsa.edu>
2016-02-12 21:32:46 -0600

    Added dynamic sql convenience functions from the obesity project

--- df_fn.py
+++ df_fn.py
@@ -187,6 +187,41 @@ def cleanup(cnx):
       logged_execute(cnx,df_stuff.format('index')).fetchall()]
     
     
+"""
+Dynamic SQLifier?
+"""
+# should be easy to turn into aggregator UDF: just collect the args, and run ds* at the end
+
+# the core function
+def ds(lval,rval=' ',lfun=' {0} ',rfun=' {0} ',op=' ',joiner=','):
+  # check for optional args and expand as needed
+  ln = len(lval);
+  # TODO: check for mismatched list lengths, non-lists, etc.
+  # TODO: check for non-string arguments (catch and fix numeric)
+  # TODO: check for non-string lists (catch and fix numeric)
+  # for any string args, turn them into lists and extend to same length
+  if isinstance(rval,str): rval = [rval]*ln;
+  if isinstance(lfun,str): lfun = [lfun]*ln;
+  if isinstance(rfun,str): rfun = [rfun]*ln;
+  if isinstance(op,str): op = [op]*ln;
+  # turn into tuples
+  rawvals = zip(lfun,lval,op,rfun,rval);
+  # payload
+  return joiner.join([str(xx[0]).format(str(xx[1]))+\
+    str(xx[2])+str(xx[3]).format(str(xx[4])) for xx in rawvals]);
+
+# convenience wrappers
+
+# for select and order-by clauses
+def dsSel(lval,rval='',lfun=' {0} '):
+  if lfun != ' {0} ' and rval == '': rval = lval;
+  return ds(lval,rval,lfun);
+
+# for where clauses and the 'on' clauses of join statements
+def dsCond(lval,rval,joiner=' and ',op=' = ',lfun = ' {0} ',rfun=' {0} '):
+  return ds(lval,rval,lfun,rfun,op,joiner);
+
+# TODO: a general-case join wrapper
 
 
 def tprint(str,tt):
